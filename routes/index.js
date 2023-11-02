2d7a19f22e87948d7a54419b384e2cf5f3b2dbae
Nikolai Dahlin <nikolai.dahlin@hotmail.com>
2023-11-02 19:56:24 +0100

    engel

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
-var express = require('express');
-var router = express.Router();
-
+const { Router } = require("express");
+const { save } = require("../save_json");
+let favouriteNumber = require("../number.json");
+const add = require("../add");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
---
+++ save_json.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+const save = async (favNumber) => {
+  console.log("saving");
+  await s3.putObject({
+    Body: JSON.stringify(favNumber, null, 2),
+    Bucket: "cyclic-outrageous-yak-windbreaker-eu-north-1",
+    Key: "number.json",
+  }).promise()
+};
+
+module.exports = { save };
